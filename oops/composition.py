3b3226cd58d51e224912684e61444d53a52b7d43
Shashwat Singh <shashwat.singh.cs@gmail.com>
2026-03-19 02:57:08 +0530

    Understood Composition

---
+++ oops/composition.py
@@ -0,0 +1,24 @@
+class Engine:
+    def __init__(self,horse_power):
+        self.horse_power = horse_power
+
+
+class Wheel:
+    def __init__(self, size):
+        self.size = size
+
+
+class Car:
+    def __init__(self, make, model,horse_power, wheel_size):
+        self.make = make
+        self.model = model
+        self.engine = Engine(horse_power)
+        self.wheels = [Wheel(wheel_size)for wheel in range(4)]
+    def display_car(self):
+        return f"{self.make} {self.model}, {self.engine.horse_power} HP, Wheel size {self.wheels[0].size}"
+
+
+car1=Car("Ford","Mustang",500,18)
+car2=Car("Ferrari","F40",478,17)
+
+print(car1.display_car())
